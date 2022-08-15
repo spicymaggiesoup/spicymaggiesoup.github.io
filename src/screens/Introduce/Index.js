e478482fb2cb13a865998189010e876bec85d4b3
spicymaggiesoup <samantha8009@gmail.com>
2022-08-15 22:35:14 +0900

    commit

---
+++ src/screens/Introduce/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Index = () => {
+  return (
+    <>
+        <h1>Introduce</h1>
+    </>
+  );
+};
+
+export default Index;
--- src/screens/Navbar.js
+++ src/screens/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Introduce from './Introduce/Index';
+import Home from './Home';
 
 const Navigation = styled.nav`
   display:flex;
@@ -7,11 +10,28 @@ const Navigation = styled.nav`
 `;
 
 const Navbar = () => {
+
+  const movePath = () => {
+    console.log('콘솔 clicked... Introduce');
+  };
+
   return (
     <Navigation>
-      <ul>Introduce</ul>
-      <ul>Blog</ul>
-      <ul>Resouces</ul>
+      <Router>
+        <ul
+          style={{cursor:"pointer"}}>
+          <Link to={"/Introduce"} className="nav-link">
+            Introduce
+          </Link>
+        </ul>
+        <ul>Blog</ul>
+        <ul>Resouces</ul>
+
+        <Routes>
+          <Route exact path="/" element={Home}></Route>
+          <Route path="/Introduce" element={Introduce}></Route>
+        </Routes>
+      </Router>
     </Navigation>
   );
 };
