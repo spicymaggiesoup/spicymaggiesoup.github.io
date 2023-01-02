26a77ef8271228ed5fd17dbc618f2636f494a0d7
spicymaggiesoup <35284787+spicymaggiesoup@users.noreply.github.com>
2023-01-02 21:38:09 +0900

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from "react-dom/client";
-import App from "./src/App";
-
-const container = document.getElementById("root");
-const root = createRoot(container as Element);
-
-root.render(<App/>);
