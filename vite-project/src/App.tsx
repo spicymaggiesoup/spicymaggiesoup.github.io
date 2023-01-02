8bc10b96bc039936f71008168d5b10f5d6438f5c
spicymaggiesoup <samantha8009@gmail.com>
2023-01-02 23:50:34 +0900

    1) setup an yarn project  2)routes products pages

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import { routes } from './routes';
+
+const App = () => {
+  const elem = useRoutes(routes);
+  return elem;
+}
+
+export default App
